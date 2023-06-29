9d1422101b4ff4ff2614eaaf7b079ce19fb4e909
randomblob <138103503+randomblob@users.noreply.github.com>
2023-06-29 13:53:47 +0530

    Commit Separately

--- main.py
+++ main.py
@@ -84,7 +84,8 @@ def push_changes():
     # Config git
     # os.system("git config --local user.name $USERNAME")
     # os.system("git config --local user.email $EMAIL")
-    os.system("git commit -am 'Update Website'")
+    os.system("git add .")
+    os.system("git commit -m 'Update Website'")
     # send a message with the changes url if any change
     hashes = get_git_revisions_hash()
     hash1 = hashes[0][:7]
